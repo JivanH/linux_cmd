c991f6277a0f3075fe246a1bd02a772aa0eda440
Anna55515 <anna.kizoxyan5@mail.com>
2021-11-05 22:38:06 +0400

    commands

--- cp/cp.c
+++ cp/cp.c
@@ -1,3 +1,16 @@
-int main() {
-  return 0;
+#include <stdio.h>
+int main(int argc, char ** argv)
+{
+if (argc!=3)
+{
+return 1;}
+FILE *fp1,*fp2;
+fp1=fopen(argv[1],"r");
+fp2=fopen(argv[2],"w");
+char c;
+while((c=fgetc(fp1))!=EOF){
+fputc(c,fp2);}
+fclose(fp1);
+fclose(fp2);
+
 }
--- mv/mv.c
+++ mv/mv.c
@@ -1,3 +1,17 @@
-int main() {
-  return 0;
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int main (int argc , char ** argv){
+if (argc!=3)
+{
+printf("wrong count ");
+return 1;}
+int result=rename(argv[1],argv[2]);
+if (result==0)
+{
+printf("The file is renamed successfully\n.");}
+else{
+printf("The file could not be renamed\n.");}
+return 0;
 }
