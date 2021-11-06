9a3bae0c4d35a76f41277beb6a87c81a119b27b4
InesaAficaryan <inesa13u@mail.ru>
2021-11-07 00:02:28 +0400

    update

--- cd/cd.c
+++ cd/cd.c
@@ -1,3 +1,30 @@
-int main() {
-  return 0;
-}
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc, char** argv)
+{
+   printf("argc = %d\n", argc);
+   
+   if (argc != 2)
+   {
+    printf("ERROR");
+    return 1;
+   }
+   
+   int result = chdir(argv[1]);
+   
+   if (result != 0)
+   {
+    printf ("Cannot change directory to \'%s\'\n", argv[1]);
+   }
+   
+   else
+   { 
+    printf ("OK\n");
+   }
+   
+   return 0;
+   
+}   
+
+    
--- cp/cp.c
+++ cp/cp.c
@@ -1,3 +1,26 @@
-int main() {
-  return 0;
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#define BUFSIZE 1024
+int main(int argc, char** argv)
+{
+if(argc != 3)
+{
+printf("ERROR");
+return 1;
+
 }
+    FILE *fp1, *fp2;
+    char buf[1024];
+    int pos;
+    fp1 = popen(argv[1], "r");
+    fp2 = popen(argv[2], "w");
+
+    while((pos=read(fp1, &buf, 1024)) != 0)
+    {
+        write(fp2, &buf, 1024);
+    }
+
+
+    return 0;
+    }
